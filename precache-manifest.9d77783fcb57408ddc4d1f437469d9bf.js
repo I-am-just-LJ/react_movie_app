13381f6e4ccf60f92f451995e6e9e01751d319e8
LJ <jhlee9193@gmail.com>
2020-10-23 18:35:51 +0900

    Updates

---
+++ precache-manifest.9d77783fcb57408ddc4d1f437469d9bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d94ec79f146b29d4a92a27a6b195eaff",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "dd886456ac149aaa9b87",
+    "url": "/react_movie_app/static/css/main.92c53a33.chunk.css"
+  },
+  {
+    "revision": "2a0fc5e7b8f8a36cf62a",
+    "url": "/react_movie_app/static/js/2.6edc61af.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/react_movie_app/static/js/2.6edc61af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd886456ac149aaa9b87",
+    "url": "/react_movie_app/static/js/main.19044f68.chunk.js"
+  },
+  {
+    "revision": "b2eab6cde90deae0fa53",
+    "url": "/react_movie_app/static/js/runtime-main.e749ff8d.js"
+  }
+]);
